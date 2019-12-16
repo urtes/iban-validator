ef44627c8edc0843fdbb1f569cf5984270afe456
Urte <urte.rimsaite@gmail.com>
2019-12-16 18:13:52 +0200

    Correct basic IBAN validation

--- src/main/java/com/urte/ibanvalidator/service/OutputService.java
+++ src/main/java/com/urte/ibanvalidator/service/OutputService.java
@@ -1,4 +1,28 @@
 package com.urte.ibanvalidator.service;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+
 public class OutputService {
+
+    public final static String OUT_EXTENSION = ".out";
+
+    public void outputValidationResult(List<String> validatedIbans, String inFilePath) {
+        File outputFile = new File(getOutFilePath(inFilePath));
+        try {
+            PrintWriter printWriter = new PrintWriter(outputFile);
+            validatedIbans.forEach(iban -> printWriter.println(iban));
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getOutFilePath(String inFilePath) {
+        int indexOfDot = inFilePath.lastIndexOf('.');
+        String name = inFilePath.substring(0, indexOfDot);
+        return inFilePath.substring(0, indexOfDot) + OUT_EXTENSION;
+    }
 }
--- src/main/java/com/urte/ibanvalidator/service/ValidationService.java
+++ src/main/java/com/urte/ibanvalidator/service/ValidationService.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.lang.Character.getNumericValue;
@@ -24,10 +26,17 @@ public class ValidationService {
                 && isAlphaNumeric(iban)) {
             String modifiedIban = (iban.substring(4) + iban.substring(0, 4));
             StringBuilder numericIban = new StringBuilder();
-            modifiedIban.chars().forEach(character -> numericIban.append(appendChar(character)));
+            for (int i = 0; i < modifiedIban.length(); i++) {
+                numericIban.append(Character.getNumericValue(modifiedIban.charAt(i)));
+            }
             BigInteger ibanNumber = new BigInteger(numericIban.toString());
-            System.out.println(IBAN_IS_VALID);
-            return ibanNumber.mod(DIVIDER).intValue() == 1;
+            if (ibanNumber.mod(DIVIDER).intValue() == 1) {
+                System.out.println(iban + IBAN_IS_VALID);
+                return true;
+            } else {
+                System.out.println(iban + IBAN_IS_INVALID);
+                return false;
+            }
         } else {
             System.out.println(iban + IBAN_IS_INVALID);
             return false;
@@ -36,10 +45,13 @@ public class ValidationService {
 
     public void validate(File inputFile) {
         try (Stream<String> ibans = Files.lines(Paths.get(inputFile.getPath()))) {
-            ibans
-                    .map(iban -> iban.trim())
+            List<String> validatedIbans = ibans
+                    .map(String::trim)
                     .filter(iban -> !iban.isEmpty())
-                    .forEach(iban -> validate(iban));
+                    .map(iban -> iban + ";" +  validate(iban))
+                    .collect(Collectors.toList());
+            OutputService outputService = new OutputService();
+            outputService.outputValidationResult(validatedIbans, inputFile.getPath());
         } catch (IOException e) {
             e.printStackTrace();
         }
