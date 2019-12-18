b39f1426c79ef5dad81dad0a2ee85a5d56abc2e2
Urte <urte.rimsaite@gmail.com>
2019-12-18 15:19:16 +0200

    Test user input validation

--- cli-app/src/test/java/com/urte/cliapp/AppTest.java
+++ cli-app/src/test/java/com/urte/cliapp/AppTest.java
@@ -1,20 +1,50 @@
 package com.urte.cliapp;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void exitsIfArgsLengthIncorrect() {
+        exit.expectSystemExitWithStatus(1);
+        exit.checkAssertionAfterwards(() -> assertEquals("Please enter mode and data\n",
+                systemOutRule.getLogWithNormalizedLineSeparator()));
+        App.main(new String[]{"A"});
+    }
+
+    @Test
+    public void exitsIfCannotParseMode() {
+        exit.expectSystemExitWithStatus(1);
+        exit.checkAssertionAfterwards(() -> assertEquals("Invalid mode\n",
+                systemOutRule.getLogWithNormalizedLineSeparator()));
+        App.main(new String[]{"A", "B"});
+    }
+
+    @Test
+    public void exitsIfModeInvalid() {
+        exit.expectSystemExitWithStatus(1);
+        exit.checkAssertionAfterwards(() -> assertEquals("Invalid mode\n",
+                systemOutRule.getLogWithNormalizedLineSeparator()));
+        App.main(new String[]{"0", "B"});
+    }
+
+    @Test
+    public void exitsIfFileNotFound() {
+        exit.expectSystemExitWithStatus(1);
+        exit.checkAssertionAfterwards(() -> assertEquals("File not found\n",
+                systemOutRule.getLogWithNormalizedLineSeparator()));
+        App.main(new String[]{"2", "B"});
     }
 }
