8fdacef61f2e57a5424689cc61323f8b8f5da99f
Urte <urte.rimsaite@gmail.com>
2019-12-18 15:57:03 +0200

    Test web-app validation service

---
+++ web-app/src/test/java/com/urte/webapp/ValidationServiceTest.java
@@ -0,0 +1,25 @@
+package com.urte.webapp;
+
+import com.urte.engine.Validator;
+import com.urte.webapp.service.ValidationService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class ValidationServiceTest {
+
+    Validator mockValidator = Mockito.mock(Validator.class);
+    ValidationService validationService = new ValidationService(mockValidator);
+
+    @Test
+    public void testValidate() {
+        validationService.validate(new ArrayList<>(Arrays.asList("one", "two")));
+        verify(mockValidator, times(2)).isValid(anyString());
+    }
+}
